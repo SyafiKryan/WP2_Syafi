1be06c954d28f8636a67c0e5d7be13c4cdab916c
SyafiKryan <syafikryanhakim@gmail.com>
2021-09-20 21:19:45 +0700

    Menambahkan file Identitas.php

---
+++ Syafikryan/Identitas.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers\Syafikryan;
+
+use App\Controllers\BaseController;
+
+class Identitas extends BaseController
+{
+    public function index()
+    {
+        echo    "<p><h2>Hola Compadres</h2></p>";
+        echo    "<p>Nama saya Syafi Kryan Hakim</p>
+                <p>Saya tinggal di Bekasi</p>
+                <p>Saya tidak suka olahraga</p>
+                <p>Saya suka menggambar</p>";
+    }
+}
