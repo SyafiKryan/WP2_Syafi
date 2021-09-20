d82536828a7301625454a774f6805244635fb0d7
SyafiKryan <syafikryanhakim@gmail.com>
2021-09-20 20:43:10 +0700

    Menambahkan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo    "<p><h2>Perkenalkan</h2></p>";
+        echo    "<p>Nama saya Syafi Kryan Hakim</p>
+                <p>Saya tinggal di Bekasi</p>
+                <p>Saya tidak suka olahraga</p>";
+    }
+}
